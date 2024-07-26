2cd4d0da51ee3074dc2d83d5a409ad662548027e
aktive <dragonapp2000@gmail.com>
2024-07-26 02:00:49 +0200

    Initial Commit

---
+++ Chapter1/Chapter1App.swift
@@ -0,0 +1,17 @@
+//
+//  Chapter1App.swift
+//  Chapter1
+//
+//  Created by aktive on 26/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct Chapter1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
